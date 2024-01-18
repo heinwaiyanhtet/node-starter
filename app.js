21af855b0a4f3c453d97c4cf5eeb694bb1e063ae
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-18 23:10:08 +0630

    added command

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("app.js")
+console.log("Starting app.js");
+
+const fs = require('fs');
+// const _ = require('load')
+
+var command = process.argv[2];
+console.log('Command: ' ,command);
+
+if(command == 'add'){
+    console.log("it is adding a new notes");
+}
+
+
