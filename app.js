a87440d6d903e849671bb00fa6c964112adad27d
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-18 23:14:40 +0630

    app.js node

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 console.log("Starting app.js");
 
 const fs = require('fs');
-// const _ = require('load')
 
 var command = process.argv[2];
 console.log('Command: ' ,command);
 
 if(command == 'add'){
     console.log("it is adding a new notes");
+} else if(command == 'list'){
+    console.log("get list");
+}else{
+    console.log("command recognized");
 }
-
 
